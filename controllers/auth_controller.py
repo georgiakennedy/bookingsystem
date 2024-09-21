877950abeed2096bc46d31b53fee43c20fb32c9a
georgiakennedy <georgia.rosekennedy00@gmail.com>
2024-09-21 20:07:18 +1000

    Created Auth Controller and added Register Route

---
+++ controllers/auth_controller.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, request
+from models.user import User, user_schema
+from init import bcrypt, db
+from sqlalchemy.exc import IntegrityError
+from psycopg2 import errorcodes
+
+auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+@auth_bp.route("/register", methods=["POST"])
+def register_user():
+    try:
+        # get the data from the body of the request
+        body_data = request.get_json()
+        # create an instance of the user model
+        user = User(
+            name = body_data.get("name"),
+            email = body_data.get("email"),
+            mobile_number = body_data.get("mobile_number")
+        )
+        # hash the password
+        password = body_data.get("password")
+        if password:
+            user.password = bcrypt.generate_password_hash(password).decode("utf-8")
+        # add and commit to the DB
+        db.session.add(user)
+        db.session.commit()
+        # return acknowledgment
+        return user_schema.dump(user), 201
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            # not null violation
+            return {"error": f"{err.orig.diag.column_name} is required."}, 400
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            #unique violation
+            return {"error": "Email address is already in use"}, 400
+
+
+#@auth_bp.route("/login")
